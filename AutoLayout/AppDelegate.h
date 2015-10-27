49288ae4ded829f16240f74856d55099ebdcb9f6
srohani <srohani@gmail.com>
2015-10-27 15:36:29 -0700

    Initial Commit

---
+++ AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Samer Rohani on 2015-10-27.
+//  Copyright © 2015 Samer Rohani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Samer Rohani on 2015-10-27.
+//  Copyright © 2015 Samer Rohani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
